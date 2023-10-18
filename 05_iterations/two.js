e79e239a095a1606c8cb239f5a6fd4dde3aea937
yash gupta <140940344+YashCommits@users.noreply.github.com>
2023-10-18 18:34:38 +0530

    while an do while loop completed

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+index=0;
+
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index=index+2
+}
+
+let score=1;
+// this loop always runs atleast ones
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=10);
+
