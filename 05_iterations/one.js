9790199c84dc7d1f32daf847f492c9a993e3d429
yash gupta <140940344+YashCommits@users.noreply.github.com>
2023-10-18 18:21:43 +0530

    for loop completed

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -4,3 +4,23 @@ for (let i = 0; i <= 10; i++) {
     const element = i;
     console.log(element);
 }
+
+//break and continue
+
+for (let index = 1; index <=20 ; index++) {
+    if (index==5){
+        console.log(`detected 5`);
+        break;
+    }
+    console.log(`value of index is ${index}`);
+}
+
+
+for (let index = 1; index <=20 ; index++) {
+    if (index==5){
+        console.log(`detected 5`);
+        continue;
+    }
+    console.log(`value of index is ${index}`);
+}
+
